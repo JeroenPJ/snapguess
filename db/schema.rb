a9148b085add96b80483410ccf5563f4c77ba4a7
Jeroen Jagt <jeroenpjagt@gmail.com>
2017-09-25 14:54:00 +0200

    Added styling, categories (seeds.rb), better layout, username login instead of email

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170925105641) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "username",               default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 20170925105641) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "score",                  default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   add_foreign_key "guesses", "categories"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["selfie", "filterselfie", "voedsel", "marqt", "reizen", "utrecht/cursus", "obscure muziek", "serie"]
+categories.each { |c| Category.create(name: c) }
